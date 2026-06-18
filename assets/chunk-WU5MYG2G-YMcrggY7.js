fe72976900854412f2632b36e05b974ed6766bc4
ipaipa10108-creator <ipaipa10108@gmail.com>
2026-06-18 19:05:48 +0800

    Updates

---
+++ assets/chunk-WU5MYG2G-YMcrggY7.js
@@ -0,0 +1,1 @@
+import{h as e,p as t}from"./src-B6vDVa-1.js";import{x as n}from"./chunk-CSCIHK7Q-p4Y4Nlai.js";var r=e(e=>{let{securityLevel:r}=n(),i=t(`body`);return r===`sandbox`&&(i=t((t(`#i${e}`).node()?.contentDocument??document).body)),i.select(`#${e}`)},`selectSvgElement`);export{r as t};
